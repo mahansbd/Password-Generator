61472100ce2822c468c42219bcf6d79aa3e2a29b
mahansbd <143232255+mahansbd@users.noreply.github.com>
2024-04-19 18:40:13 -0400

    Fianl Com

--- PassGen.py
+++ PassGen.py
@@ -1,10 +1,12 @@
 import random
 
+"Declare the characters you want in your password"
 uppercase_letters = "ABCDEFGHIJkLMNOPQRSTUVWXYZ"
 lowercase_letters = uppercase_letters.lower()
 digits = "0123456789"
 symbols = "()[]{},.:;-_/\\?*# "
 
+'''Change to false to disable characters, and change to true to enable characters.'''
 upper, lower, nums, syms = True, True, True, True
 
 all = ""
@@ -18,9 +20,13 @@ if nums:
 if syms:
     all += symbols
 
-length = 40
-amount = 20
+'''Here you can change the length of the password, and/or the amount of passwords created.'''
+length = 20
+amount = 10
+
 
 for x in range(amount):
     password = "".join(random.sample(all, length))
+
+
     print(password)
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Password-Generator
 
-This script lets you create a random complex password.
+This script lets you create a random complex password. Useful for creating more secure passwords.
 
-Instructions. 1. on line 8 you can specify which type of characters you want in your password by changing the true to false to disable characters.
+Instructions. 
+1. on line 8 you can specify which type of characters you want in your password by changing the true to false to disable characters.   
+2. You can change the length of the password and amount of passwaords created by editing the "lenght", or the "amount" portion.
+3. Run the code and Password will be generated.
